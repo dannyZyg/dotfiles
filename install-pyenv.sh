b97736184ee945cc3b7face2fa71e24ffd483b7e
Danny Keig <danny@s1t2.com.au>
2020-11-19 22:00:29 +1100

    Fix pyenv install script

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 curl https://pyenv.run | bash
-exec "$SHELL"
 
-pyenv install 2.7.18 \
-	& pyenv global 3.9.0 \
-	& $(pyenv which python3) -m pip install virtualenvwrapper
+export PYENV_ROOT=$HOME/.pyenv
+export PATH="$HOME/.pyenv/bin:$PATH"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init -)"
+	eval "$(pyenv virtualenv-init -)"
+fi
+
+pyenv install -f 2.7.18 \
+	&& pyenv install -f 3.9.0 \
+	&& pyenv global 3.9.0 \
+	&& $(pyenv which python3) -m pip install virtualenvwrapper
