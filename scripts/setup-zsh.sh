0f0e4e8e2de66685c010bab89fae7beea50d260e
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2021-07-13 22:24:03 +1000

    Edit zsh setup with check for arm/x86

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -3,19 +3,31 @@
 echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile.
+BREWPATH=''
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	CPU=$(uname -p)
+	if [[ "$CPU" == "arm" ]]; then
+		BREWPATH="/opt/homebrew/bin"
+	else
+		BREWPATH="/usr/local/bin"
+	fi
+fi
+
+ZSHPATH="$BREWPATH/zsh"
 
 # https://stackoverflow.com/a/4749368/1341838
-if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
-  echo '/usr/local/bin/zsh already exists in /etc/shells'
+if grep -Fxq $ZSHPATH '/etc/shells'; then
+  echo "$ZSHPATH already exists in /etc/shells"
 else
   echo "Enter superuser (sudo) password to edit /etc/shells"
-  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  echo $ZSHPATH | sudo tee -a '/etc/shells' >/dev/null
 fi
 
 
-if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
-  echo '$SHELL is already /usr/local/bin/zsh'
+if [ "$SHELL" = "$ZSHPATH" ]; then
+  echo "\$SHELL is already $ZSHPATH"
 else
   echo "Enter user password to change login shell"
-  chsh -s '/usr/local/bin/zsh'
+  chsh -s $ZSHPATH
 fi
