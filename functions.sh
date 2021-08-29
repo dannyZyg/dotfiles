b4ee95202f66136f33104756ce4eeed8e2bb05fc
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2021-08-29 17:28:40 +1000

    Add function to install/load zsh plugins

--- functions.sh
+++ functions.sh
@@ -99,3 +99,14 @@ function pyenv-rm() {
 function exists() {
 	command -v $1 >/dev/null 2>&1
 }
+
+function zsh_add_plugin() {
+    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
+    if [ -d "$ZDOTDIR/plugins/$PLUGIN_NAME" ]; then
+        # For plugins
+        source "$ZDOTDIR/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
+        source "$ZDOTDIR/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
+    else
+        git clone "https://github.com/$1.git" "$ZDOTDIR/plugins/$PLUGIN_NAME"
+    fi
+}
