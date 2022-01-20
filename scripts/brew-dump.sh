ce1e0222a360d66a98facde3a9cd132b8828f863
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-01-20 12:21:46 +1100

    Fix redirect in brew dump script

--- scripts/brew-dump.sh
+++ scripts/brew-dump.sh
@@ -7,7 +7,7 @@ brew_packages=$(echo "$brew_dump" | grep 'brew ')
 cask_packages=$(echo "$brew_dump" | grep 'cask ')
 mas_packages=$(echo "$brew_dump" | grep 'mas ')
 
-echo "$taps" >> ./packages/homebrew/taps
-echo "$brew_packages" >> ./packages/homebrew/brew_packages
-echo "$cask_packages" >> ./packages/homebrew/casks
-echo "$mas_packages" >> ./packages/homebrew/mas
+echo "$taps" > ./packages/homebrew/taps
+echo "$brew_packages" > ./packages/homebrew/brew_packages
+echo "$cask_packages" > ./packages/homebrew/casks
+echo "$mas_packages" > ./packages/homebrew/mas
