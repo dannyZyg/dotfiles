a881ffc56c469365cf16c1fc8231692216131fde
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-04-30 17:30:34 +1000

    Add lualine

--- nvim/lua/dzk/plugins.lua
+++ nvim/lua/dzk/plugins.lua
@@ -51,7 +51,10 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-tree.lua"
   -- use "akinsho/bufferline.nvim"
   -- use "moll/vim-bbye"
-  -- use "nvim-lualine/lualine.nvim"
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
   -- use "akinsho/toggleterm.nvim"
   -- use "ahmedkhalf/project.nvim"
   -- use "lewis6991/impatient.nvim"
