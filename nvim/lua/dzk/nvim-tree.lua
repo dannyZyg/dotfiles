b840941fab373ae916db0cfa1586b0add8a8a69b
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-05-30 14:14:27 +1000

    Update nvim-tree config after update

--- nvim/lua/dzk/nvim-tree.lua
+++ nvim/lua/dzk/nvim-tree.lua
@@ -1,25 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -64,6 +44,7 @@ nvim_tree.setup {
     },
   },
   renderer = {
+    highlight_opened_files = "all",
     indent_markers = {
       enable = false,
       icons = {
@@ -74,6 +55,26 @@ nvim_tree.setup {
     },
     icons = {
       webdev_colors = true,
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      }
     },
   },
   hijack_directories = {
