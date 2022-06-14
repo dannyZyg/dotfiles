6bb477ecbceaef2c92b156319e9b935d14912e0a
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-06-14 16:02:02 +1000

    Fix pylsp config

--- nvim/lua/dzk/lsp/servers/pylsp.lua
+++ nvim/lua/dzk/lsp/servers/pylsp.lua
@@ -1,19 +1,23 @@
 local M = {}
 
 M.config = {
-  cmd = { "pylsp" },
-  filetypes = { "python" },
-  single_file_support = true,
-  plugins = {
-    configurationSources = {"flake8"},
-    pycodestyle = {enabled = false},
-    flake8 = {enabled = true},
-    mypy = {
-      enabled = true,
-      live_mode =true,
-      strict = true
-    },
-  },
+  settings = {
+    pylsp = {
+      cmd = { "pylsp" },
+      filetypes = { "python" },
+      single_file_support = true,
+      plugins = {
+        configurationSources = {"flake8"},
+        pycodestyle = {enabled = false},
+        flake8 = {enabled = true},
+        mypy = {
+          enabled = true,
+          live_mode =true,
+          strict = true
+        },
+      },
+    }
+  }
 }
 
 return M
