ec547cb5e97750c6eee896777c0909aa258a7c9e
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-07-15 19:05:48 +1000

    Add application spawns for refactored rc

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -101,3 +101,11 @@ awful.rules.rules = main.rules(
 
 -- {{{ Signals }}} --
 require("main.signals")
+
+awful.spawn.with_shell("sxhkd")
+awful.spawn.with_shell("copyq")
+awful.spawn.with_shell("flameshot")
+awful.spawn.with_shell("nm-applet")
+awful.spawn.with_shell("picom")
+awful.spawn.with_shell("1password --silent")
+awful.spawn.with_shell("key-remaps")
