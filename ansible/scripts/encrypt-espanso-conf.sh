25fa1625544fd97f2061a9e33e5e32afa0463e95
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-07-17 10:35:20 +1000

    Fix espanso install

--- ansible/scripts/encrypt-espanso-conf.sh
+++ ansible/scripts/encrypt-espanso-conf.sh
@@ -12,4 +12,4 @@ ESPANSO_DIR=$(espanso path | awk '{print $2}' | head -n 1)
 
 cp -r $ESPANSO_DIR .
 
-ansible-vault encrypt --vault-id $VAULT_ID@prompt espanso/match/* espanso/config/*
+ansible-vault encrypt --vault-id $VAULT_ID@prompt espanso/match/*.yml espanso/config/*.yml
