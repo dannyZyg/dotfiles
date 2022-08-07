2a6e18d4f63007602687870cd4ac54f7f2318b68
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2022-08-08 08:05:43 +1000

    Fix zoom rule

--- awesome/main/rules.lua
+++ awesome/main/rules.lua
@@ -28,27 +28,26 @@ function _M.get(clientkeys, clientbuttons)
 			},
 		},
 
-		-- Floating clients.
-		{
-			rule_any = {
-				instance = {
-					"DTA", -- Firefox addon DownThemAll.
-					"copyq", -- Includes session name in class.
-					"pinentry",
-				},
-				class = {
-					"Arandr",
-					"Blueman-manager",
-					"Gpick",
-					"Kruler",
-					"MessageWin", -- kalarm.
-					"Sxiv",
-					"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-					"Wpa_gui",
-					"veromix",
-					"xtightvncviewer",
-					"zoom.real",
-				},
+    -- Floating clients.
+    { rule_any = {
+        instance = {
+          "DTA",  -- Firefox addon DownThemAll.
+          "copyq",  -- Includes session name in class.
+          "pinentry",
+        },
+        class = {
+          "Arandr",
+          "Blueman-manager",
+          "Gpick",
+          "Kruler",
+          "MessageWin",  -- kalarm.
+          "Sxiv",
+          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+          "Wpa_gui",
+          "veromix",
+          "xtightvncviewer",
+          "zoom"
+        },
 
 				-- Note that the name property shown in xprop might be set slightly after creation of the client
 				-- and the name shown there might not match defined rules here.
