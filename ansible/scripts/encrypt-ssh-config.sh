a1496101a3ad3943ff84946ba2377e2d48ea36e9
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2023-02-07 11:45:39 +1100

    Move ssh config to role files

--- ansible/scripts/encrypt-ssh-config.sh
+++ ansible/scripts/encrypt-ssh-config.sh
@@ -4,4 +4,4 @@ VAULT_ID=ssh-config
 
 cp ~/.ssh/config ./ssh/config
 
-ansible-vault encrypt --vault-id $VAULT_ID@prompt ./ssh/config
+ansible-vault encrypt --vault-id $VAULT_ID@prompt ./roles/ssh/files/config
