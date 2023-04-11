73ded90f60b4c1724910ea79cb28f7cae4c4d51c
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2023-04-11 15:35:15 +1000

    Update nvim plugins

--- nvim/lua/dzk/lsp/servers/lua_ls.lua
+++ nvim/lua/dzk/lsp/servers/lua_ls.lua
@@ -8,8 +8,6 @@ M.config = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
