53a454805a23b42204a2aa2ef255d87dcc118702
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2023-06-21 10:23:02 +1000

    Add pyright root_dir config

--- nvim/lua/dzk/lsp/servers/pyright.lua
+++ nvim/lua/dzk/lsp/servers/pyright.lua
@@ -1,8 +1,20 @@
 local M = {}
 
+local util = require("lspconfig.util")
+
+local root_files = {
+	"pyproject.toml",
+	"setup.py",
+	"setup.cfg",
+	"requirements.txt",
+	"Pipfile",
+	"pyrightconfig.json",
+}
+
 M.config = {
 	cmd = { "pyright-langserver", "--stdio" },
 	filetypes = { "python" },
+	root_dir = util.root_pattern(unpack(root_files)),
 	settings = {
 		python = {
 			analysis = {
