808ef9c09b9e882d5e80402329fe0898e4e35dcd
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2023-09-02 16:15:40 +1000

    Fix nvim tree

--- nvim/lua/dzk/nvim-tree.lua
+++ nvim/lua/dzk/nvim-tree.lua
@@ -6,15 +6,8 @@ if not status_ok then
 	return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
-end
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
 
 local function on_attach(bufnr)
 	local api = require("nvim-tree.api")
