bf6a10616f99820a29a7236d608a27c044d0df84
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2023-12-28 22:24:00 +1100

    Rev window resizing

--- nvim/lua/dzk/keymaps.lua
+++ nvim/lua/dzk/keymaps.lua
@@ -32,8 +32,8 @@ vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>!", ":q!<CR>")
 
 -- Resize with arrows
-vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize +2<CR>")
 
