24879d7e65ccad65c55e250718489754921744cf
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2024-08-22 14:40:51 +1000

    Add gruvbox back

--- nvim/lua/dzk/colorscheme.lua
+++ nvim/lua/dzk/colorscheme.lua
@@ -1,4 +1,7 @@
 vim.cmd("colorscheme default")
 
-vim.o.background = "dark" -- or 'light'
-vim.cmd.colorscheme("tokyonight")
+--[[ vim.o.background = "dark" -- or 'light' ]]
+--[[ vim.cmd.colorscheme("tokyonight") ]]
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
--- nvim/lua/dzk/plugins.lua
+++ nvim/lua/dzk/plugins.lua
@@ -57,6 +57,7 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	use("folke/tokyonight.nvim")
+	use("ellisonleao/gruvbox.nvim")
 
 	-- cmp plugins
 	use("hrsh7th/nvim-cmp") -- The completion plugin
