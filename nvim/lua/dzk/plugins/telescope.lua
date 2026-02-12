bba0708a5a956237f7b74d26b04cd7c3717c0465
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2026-02-12 13:40:03 +1100

    Run opts as func

--- nvim/lua/dzk/plugins/telescope.lua
+++ nvim/lua/dzk/plugins/telescope.lua
@@ -14,15 +14,14 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     enabled = true,
-    opts = {
-      defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
-        path_display = { "absolute" },
-        mappings = (function()
-
-          local actions = require("telescope.actions")
-          return {
+    opts = function()
+      local actions = require("telescope.actions")
+      return {
+        defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
+          path_display = { "absolute" },
+          mappings = {
             i = {
               ["<C-n>"] = actions.cycle_history_next,
               ["<C-p>"] = actions.cycle_history_prev,
@@ -85,41 +84,41 @@ return {
 
               ["?"] = actions.which_key,
             },
-          }
-        end)(),
-      },
-      pickers = {
-        -- Default configuration for builtin pickers goes here:
-        -- picker_name = {
-        --   picker_config_key = value,
-        --   ...
-        -- }
-        -- Now the picker_config_key will be applied every time you call this
-        -- builtin picker
-      },
-      extensions = {
-        -- media_files = {
-        --   -- filetypes whitelist
-        --   -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        --   filetypes = {"png", "webp", "jpg", "jpeg"},
-        --   find_cmd = "rg" -- find command (defaults to `fd`)
-        -- },
-        fzf = {
-          fuzzy = true, -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
         },
-        -- Your extension configuration goes here:
-        -- extension_name = {
-        --   extension_config_key = value,
-        -- }
-        -- please take a look at the readme of the extension you want to configure
-      },
-    },
+        pickers = {
+          -- Default configuration for builtin pickers goes here:
+          -- picker_name = {
+          --   picker_config_key = value,
+          --   ...
+          -- }
+          -- Now the picker_config_key will be applied every time you call this
+          -- builtin picker
+        },
+        extensions = {
+          -- media_files = {
+          --   -- filetypes whitelist
+          --   -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+          --   filetypes = {"png", "webp", "jpg", "jpeg"},
+          --   find_cmd = "rg" -- find command (defaults to `fd`)
+          -- },
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
+          -- Your extension configuration goes here:
+          -- extension_name = {
+          --   extension_config_key = value,
+          -- }
+          -- please take a look at the readme of the extension you want to configure
+        },
+      }
+    end,
 
-    init = function ()
-      telescope = require("telescope")
+    init = function()
+      local telescope = require("telescope")
       telescope.load_extension("fzf")
       telescope.load_extension("scdoc")
     end,
