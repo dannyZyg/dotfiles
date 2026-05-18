f8b7ce04bb0484af34e58afa0f99cbc0de04e58c
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2026-05-18 14:10:55 +1000

    Simplify treesitter conf

--- nvim/lua/dzk/plugins/treesitter.lua
+++ nvim/lua/dzk/plugins/treesitter.lua
@@ -1,24 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    enabled = true,
     build = ":TSUpdate",
     config = function()
-      -- Register genexpr filetype (custom parser - must be installed manually via tree-sitter CLI)
-      vim.filetype.add({ extension = { genexpr = "genexpr" } })
-
-      -- New API: just set install directory if needed
-      require("nvim-treesitter").setup({
-        -- Uses default install_dir: vim.fn.stdpath('data')/site
-      })
-
-      -- Enable treesitter-based highlighting
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          pcall(vim.treesitter.start)
-        end,
-      })
+      vim.treesitter.language.register("markdown", "mdx")
     end,
   },
 }
