20283ca9de91f7373cfb5fcf0f532d1da5ef87b6
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2026-07-15 21:27:41 +1000

    Fix treesitter bug

--- nvim/lua/dzk/plugins/treesitter.lua
+++ nvim/lua/dzk/plugins/treesitter.lua
@@ -4,6 +4,12 @@ return {
     build = ":TSUpdate",
     config = function()
       vim.treesitter.language.register("markdown", "mdx")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
     end,
   },
 }
